d41eb6a829398e7c3cee4620649767a508f28943
Johannes Engelbrecht <johannes.engelbrecht@mixtelematics.com>
2017-08-30 12:03:06 +0200

    Add interface IIdentityClient

---
+++ MiX.Identity.Client/IIdentityClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IdentityModel.Client;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace MiX.Identity.Client
+{
+	public interface IIdentityClient
+	{
+		TokenResponse RequestToken(string username, string password, string scopes);
+		TokenResponse RefreshToken(string refreshToken);
+		JwtSecurityToken DecodeToken(string token);
+	}
+}
